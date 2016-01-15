920d711f9d6f3c461ce9d99a34249a6fcaeee21a
杨晓伟 <yxw610@github.com>
2016-01-15 14:59:00 +0800

    初始版本

---
+++ HeartTravel/HeartTravel/Source/Header/APPHeader.h
@@ -0,0 +1,13 @@
+//
+//  APPHeader.h
+//  ProjectMusic
+//
+//  Created by young on 15/7/31.
+//  Copyright (c) 2015年 young. All rights reserved.
+//  这里存放普通的app宏定义和声明等信息.
+
+#ifndef Project_APPHeader_h
+#define Project_APPHeader_h
+
+
+#endif
